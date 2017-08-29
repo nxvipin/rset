4ef8d64855a6a022bdc797678e0bb5f387fcf9ac
Vipin Nair <swvist@gmail.com>
2017-08-30 05:18:07 +0530

    Partial Set Add API (WIP)
    Currently the add operation on set affects only the set at the source
    replica. The downstream operation of add is not propagated to the other
    replicas.

--- src/rset.erl
+++ src/rset.erl
@@ -1,7 +1,8 @@
 -module(rset).
 -author("Vipin Nair <swvist@gmail.com>").
 
--export([init/2]).
+-export([init/2,
+         add/2]).
 
 -record(element, {update    :: update(),
                   timestamp :: timestamp(),
@@ -28,3 +29,30 @@ init(ThisReplica, AllReplicas) ->
           timestamp=0,
           ivvmap=maps:from_list([{Rep, []} || Rep <- AllReplicas]),
           this=ThisReplica}.
+
+add({add_downstream, {_MsgVal, MsgTimestamp, MsgSourceReplica}=Element},
+    #rset{elements=Elements,
+          ivvmap=IVVMap}=Rset) ->
+    %% Downstream operation of Add. We have not see this element before so we
+    %% add it and update our element list and ivv of the source replica.
+    #{MsgSourceReplica := MsgSourceReplicaIVV0}=IVVMap,
+    case not ivv:contains(MsgTimestamp, MsgSourceReplicaIVV0) of
+         true ->
+            #{MsgSourceReplica := MsgSourceReplicaIVV0}=IVVMap,
+            MsgSourceReplicaIVV = ivv:add(MsgTimestamp, MsgSourceReplicaIVV0),
+            Rset#rset{
+              elements = [Element | Elements],
+              ivvmap = IVVMap#{MsgSourceReplica := MsgSourceReplicaIVV}};
+        false ->
+            Rset
+    end;
+
+add(Val, #rset{this=SourceReplica, timestamp=Timestamp}=Rset) ->
+    %% A value was added at this replica. We create an element() from this value
+    %% and propagate it downstream.
+    Element = {Val, Timestamp, SourceReplica},
+
+    %% Downstream operation at this replica, which is the source replica.
+    add({add_downstream, Element}, Rset#rset{timestamp=Timestamp+1}).
+    %% [TODO]: Send downstream add operation to all replicas other than the
+    %% source replica ideally handled by a messaging middleware.
