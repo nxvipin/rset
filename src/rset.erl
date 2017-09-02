2234505fba438ff2c43069d0066abec5e975787a
Vipin Nair <swvist@gmail.com>
2017-09-02 21:55:40 +0530

    Move type definitions to a hrl file

--- src/rset.erl
+++ src/rset.erl
@@ -1,31 +1,11 @@
 -module(rset).
 -author("Vipin Nair <swvist@gmail.com>").
 
+-include("rset.hrl").
+
+%% Public API
 -export([init/2,
          add/2]).
-
--record(element, {update    :: update(),
-                  timestamp :: timestamp(),
-                  replica   :: replica()}).
-
--record(rset, {elements     :: elements(),
-               timestamp    :: timestamp(),
-               ivvmap       :: ivvmap(),
-               repinfo      :: {this_replica(),
-                                other_replicas(),
-                                all_replicas()}}).
-
--type update()         :: any().
--type timestamp()      :: non_neg_integer().
-
--type replica()        :: any().
--type this_replica()   :: replica().       %% Current replica
--type other_replicas() :: list(replica()). %% All replicas except `this` replica
--type all_replicas()   :: list(replica()). %% All participating replicas
-
--type ivvmap()         :: #{replica() := ivv:ivv()}.
--type element()        :: #element{}.
--type elements()       :: list(element()).
 
 
 %% -----------------------------------------------------------------------------
