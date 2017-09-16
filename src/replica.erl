5a60c4552ac36067209d50908fe8025e3e337417
Vipin Nair <swvist@gmail.com>
2017-09-17 01:56:12 +0530

    Make all downstream operations async with acks
    All downstream operations to replicas except the current one are now
    asynchronous. Downstream replicas on receiving a downstream operation
    will update their state and send an ack back to the source replica.
    Right now if any downstream operation fails due to a replica being
    unavailable or due to network issues, there is no retry mecahnism yet.
    Building a resilient system on top of this requires us to keep track of
    all downstream operations and their acks and retry them when necessary
    taking advantage of the fact that all operations are idempotent.

--- src/replica.erl
+++ src/replica.erl
@@ -24,6 +24,7 @@
 
 %% -----------------------------------------------------------------------------
 
+
 create(AllReplicas) ->
     rset_sup:create_replica(AllReplicas).
 
@@ -32,8 +33,19 @@ start_link(ThisReplica, AllReplicas) ->
     gen_server:start_link({local, ThisReplica}, ?MODULE,
                           [ThisReplica, AllReplicas], []).
 
+
+add(Replica, {_,_,_}=Element) ->
+    %% Downstream operations are async. This is called by the source replica to
+    %% propagate downstream add operation.
+    gen_server:cast(Replica, {add_downstream, Element});
+
 add(Replica, Value) ->
     gen_server:call(Replica, {add, Value}).
+
+delete(Replica, {ThisReplica, #{}=DelIVVMap}) ->
+    %% Downstream operations are async. This is called by the source replica to
+    %% propagate downstream delete operation.
+    gen_server:cast(Replica, {delete_downstream, ThisReplica, DelIVVMap});
 
 delete(Replica, Value) ->
     gen_server:call(Replica, {delete, Value}).
@@ -41,27 +53,35 @@ delete(Replica, Value) ->
 elements(Replica) ->
     gen_server:call(Replica, elements).
 
+ack(SourceReplica, DownstreamReplica, {_,_,_}=Element) ->
+    %% Ack a downstream add message received by the
+    gen_server:cast(SourceReplica, {ack_add_downstream, SourceReplica,
+                                    DownstreamReplica, Element});
+
+ack(SourceReplica, DownstreamReplica, #{}=DelIVVMap) ->
+    %% Ack a downstream delete message received by the
+    gen_server:cast(SourceReplica, {ack_add_downstream, SourceReplica,
+                                    DownstreamReplica, DelIVVMap}).
+
 init([ThisReplica, AllReplicas]) ->
     {ok, rset:init(ThisReplica, AllReplicas)}.
 
-handle_call({add, {_, _, _}=Element}, _From, State0) ->
-    {Element, State} = rset:add(Element, State0),
-    {reply, {ok, Element}, State};
+handle_call({add, Value}, From,
+            #rset{repinfo = {ThisReplica, OtherReplicas, _}}=State0) ->
 
-handle_call({add, Value}, _From,
-            #rset{repinfo = {_, OtherReplicas, _}}=State0) ->
-    {Element, State} = rset:add(Value, State0),
+    {{Value, Timestamp, ThisReplica}=Element, State} = rset:add(Value, State0),
+    lager:debug("Add operation received from Client: ~p "
+                "ThisReplica: ~p Timestamp: ~p Value:~p",
+                [From, ThisReplica, Timestamp, Value]),
     [add(Replica, Element) || Replica <- OtherReplicas],
     {reply, {ok, Element}, State};
 
-handle_call({delete, #{}=DelIVVMap}, _From, State0) ->
-    {Element, State} = rset:delete(DelIVVMap, State0),
-    {reply, {ok, Element}, State};
-
-handle_call({delete, Value}, _From,
-            #rset{repinfo = {_, OtherReplicas, _}}=State0) ->
+handle_call({delete, Value}, From,
+            #rset{repinfo = {ThisReplica, OtherReplicas, _}}=State0) ->
     {DelIVVMap, State} = rset:delete(Value, State0),
-    [delete(Replica, DelIVVMap) || Replica <- OtherReplicas],
+    lager:debug("Delete operation received from Client: ~p ThisReplica: ~p "
+                "DelIVVMap:~p", [From, ThisReplica, DelIVVMap]),
+    [delete(Replica, {ThisReplica, DelIVVMap}) || Replica <- OtherReplicas],
     {reply, {ok, DelIVVMap}, State};
 
 handle_call(elements, _From, State) ->
@@ -71,6 +91,38 @@ handle_call(elements, _From, State) ->
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
+
+handle_cast({add_downstream, {Value, Timestamp, SourceReplica}=Element},
+            #rset{repinfo={ThisReplica, _, _}}=State0) ->
+    lager:debug("Add downstream operation received from SourceReplica: ~p "
+                "ThisReplica: ~p Value: ~p Timestamp: ~p",
+                [SourceReplica, ThisReplica, Value, Timestamp]),
+    {Element, State} = rset:add(Element, State0),
+    ack(SourceReplica, ThisReplica, Element),
+    {noreply, State};
+
+handle_cast({ack_add_downstream, ThisReplica, DownstreamReplica, Element},
+            #rset{repinfo={ThisReplica, _, _}}=State) ->
+    lager:debug("Add downstream ACK received at ThisReplica: ~p "
+                "from DownstreamReplica: ~p for Element: ~p",
+                [ThisReplica, DownstreamReplica, Element]),
+    {noreply, State};
+
+handle_cast({delete_downstream, SourceReplica, #{}=DelIVVMap},
+            #rset{repinfo={ThisReplica, _, _}}=State0) ->
+    lager:debug("Delete downstream operation received from SourceReplica: ~p "
+                "ThisReplica: ~p DelIVVMap: ~p",
+                [SourceReplica, ThisReplica, DelIVVMap]),
+    {DelIVVMap, State} = rset:delete(DelIVVMap, State0),
+    ack(SourceReplica, ThisReplica, DelIVVMap),
+    {noreply, State};
+
+handle_cast({ack_delete_downstream, ThisReplica, DownstreamReplica, DelIVVMap},
+            #rset{repinfo={ThisReplica, _, _}}=State) ->
+    lager:debug("Delete downstream ACK received at ThisReplica: ~p "
+                "from DownstreamReplica: ~p for DelIVVMap: ~p",
+                [ThisReplica, DownstreamReplica, DelIVVMap]),
+    {noreply, State};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
