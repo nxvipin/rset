d8ecfc4b4c4b54113020513a4970ab745fb6ec45
Vipin Nair <swvist@gmail.com>
2017-09-24 19:49:53 +0530

    Vgarant machine updates
    - Document stuff
    - Enable erlang shell history
    - Setup passwordless SSH (this will be useful when we spawn erlang nodes
      in tests to create replicas)

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,19 @@
+# Install erlang
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install erlang -y
 rm -rf rebar3
+
+# Install rebar3
 wget https://s3.amazonaws.com/rebar3/rebar3
 chmod +x rebar3
 su - vagrant -c "./rebar3 local install"
 echo "export PATH=/home/vagrant/.cache/rebar3/bin:$PATH" >> /home/vagrant/.bashrc
+
+# Enable erlang shell history
+echo "export ERL_AFLAGS=\"-kernel shell_history enabled\"" >> /home/vagrant/.bashrc
+
+# Setup passwordless ssh (for tests)
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
